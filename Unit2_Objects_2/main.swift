131e2ec8fadf448e26ccc4f7044cb4e6d40b5aef
CodeKitchen00 <David.S.Ohana@gmail.com>
2014-09-12 12:40:40 -0400

    Initial Commit

---
+++ Unit2_Objects_2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Unit2_Objects_2
+//
+//  Created by David Ohana on 9/12/14.
+//  Copyright (c) 2014 David Ohana. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
